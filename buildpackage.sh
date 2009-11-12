228861f5ba399abaf86e02db04f64b5bb9cd4114
vincent.alquier <vincent.alquier@bdb6ae90-fe14-11dd-a320-db7d8e7a638f>
2009-11-12 17:40:57 +0000

    adding the project revision number in the build version

--- buildpackage.sh
+++ buildpackage.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
-version="0.5a"
+majorversion="0.5a"
+revision=`svnversion`
+version="$majorversion-$revision"
 builddir="linux-2.6-all"
+
+mv -f obmtranslation/core.rb obmtranslation/core.rb.bak
+sed "s/Version\ =\ '.*'/Version\ =\ '$majorversion'/" obmtranslation/core.rb.bak > obmtranslation/core.rb
+
 mv -f obmtranslation/config.rb obmtranslation/config.rb.bak
 sed "s/DataDir\ =\ '.*'/DataDir\ =\ '\/usr\/share\/obmtranslation'/" obmtranslation/config.rb.bak > obmtranslation/config.rb
+
+sed "s/%version\ .*/%version $version/" obmtranslation.list.dist > obmtranslation.list
+
 sudo epm -a all -n -f deb obmtranslation
+
+rm -f obmtranslation/core.rb.bak
 rm -f obmtranslation/config.rb
 mv -f obmtranslation/config.rb.bak obmtranslation/config.rb
+rm -f obmtranslation.list
+
 tar_dir="$builddir/obmtranslation-$version"
 rm -Rf $tar_dir
 mkdir $tar_dir
