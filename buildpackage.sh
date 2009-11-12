bb513229bcfa3c24ad414005fb1598ac175d53e9
vincent.alquier <vincent.alquier@bdb6ae90-fe14-11dd-a320-db7d8e7a638f>
2009-11-12 16:20:52 +0000

    now packaging for all debian architectures

--- buildpackage.sh
+++ buildpackage.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-version=0.5a
+version="0.5a"
+builddir="linux-2.6-all"
 mv -f obmtranslation/config.rb obmtranslation/config.rb.bak
 sed "s/DataDir\ =\ '.*'/DataDir\ =\ '\/usr\/share\/obmtranslation'/" obmtranslation/config.rb.bak > obmtranslation/config.rb
-sudo epm -n -f deb obmtranslation
+sudo epm -a all -n -f deb obmtranslation
 rm -f obmtranslation/config.rb
 mv -f obmtranslation/config.rb.bak obmtranslation/config.rb
-tar_dir=linux-2.6-intel/obmtranslation-$version
+tar_dir="$builddir/obmtranslation-$version"
 rm -Rf $tar_dir
 mkdir $tar_dir
 cp -f obmtranslation.rb $tar_dir
@@ -13,7 +14,7 @@ cp -Rf obmtranslation $tar_dir
 cp -Rf glade $tar_dir
 cp -f LICENSE $tar_dir
 sed s/DataDir\ =\ '.*'/DataDir\ =\ \'\.\'/ obmtranslation/config.rb > $tar_dir/obmtranslation/config.rb
-cd linux-2.6-intel
+cd $builddir
 tar -czf obmtranslation-$version.tgz obmtranslation-$version
 cd ..
 rm -Rf $tar_dir
